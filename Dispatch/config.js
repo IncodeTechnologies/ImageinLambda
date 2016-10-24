6e2d050beb71e5c8a5e8a0d34092fe2fd24bbacf
Alexey Golunov <alexeygolunov@Alexeys-MacBook-Air.local>
2016-10-24 13:35:59 -0700

    Cleanup

--- Dispatch/config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.redis = {};
-config.redis.host = 'nodelambdatest.14uibc.0001.usw2.cache.amazonaws.com';
-config.redis.port = 6379;
-config.redis.usersQueue = 'users';
-
-module.exports = config;
